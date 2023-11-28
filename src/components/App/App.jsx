cb2a256c6c7247a3bf577124e65a9ca73644a3f3
Brian Werner <brianmw199@gmail.com>
2023-11-27 19:59:21 -0600

    Got all of the routes working, just got to get a new student added on success

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,32 @@
 import axios from 'axios';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import StudentForm from '../StudentForm/StudentForm.jsx';
 import StudentList from '../StudentList/StudentList.jsx';
+import Nav from '../Nav/Nav.jsx';
 
 function App() {
     return (
         <div className="App">
+          <Router>
             <header className="App-header">
             <h1 className="App-title">GitHub Student List</h1>
             </header>
             <br/>
+          <Nav />
+            
+          <Route exact path="/home">
             <StudentForm />
-
-            <p>Student list:</p>
+          </Route>
+            
+          <Route exact path="/allStudents">
+           <p>Student list:</p>
             <StudentList />
+          </Route>
+          <Route excat path="/about">
+            <p>This a about page!</p>
+          </Route>
+          </Router>
         </div>
     );
 }
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+
+
+function Nav() {
+
+    return (
+            <ul>
+                <Link to="/home">
+                    <li>Home</li>
+                </Link>
+                <Link to="/allStudents">
+                    <li>Students</li>
+                </Link>
+                <Link to="/about">
+                    <li>About</li>
+                </Link>
+            </ul>
+    )
+
+}
+
+export default Nav;
